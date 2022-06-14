81a1b6767d0e755a660be6b9869b28d76340c36e
swat121 <stark2542@gmail.com>
2022-06-14 23:48:49 +0300

    Add: user interface

---
+++ Game/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(616, 616);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(358, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Player Green Walls:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(6, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(214, 33);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Player Red Walls:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(59, 660);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(516, 33);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Click on the pawn to move it Or place a wall.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(213, 5);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 33);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "_";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(599, 5);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 33);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "_";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 700);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Quoridor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Game/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        int ProbelX = 0, ProbelY = 0, Shirota = 0, StoronaKvadrata = 0, Diametr = 0, ShirotaStenki = 0;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void GrafKvadrat()
+        {
+            Shirota = pictureBox1.Width;
+            StoronaKvadrata = Shirota / 14;
+
+            pictureBox1.Image = new Bitmap(Shirota, Shirota);
+            Graphics gr = Graphics.FromImage(pictureBox1.Image);
+
+            ProbelX = StoronaKvadrata / 2;
+            ProbelY = StoronaKvadrata / 2;
+
+            for (int j = 0; j < 9; j++)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    gr.FillRectangle(Brushes.White, ProbelX, ProbelY, StoronaKvadrata, StoronaKvadrata);
+                    ProbelX = ProbelX + StoronaKvadrata / 2 + StoronaKvadrata;
+                }
+                ProbelX = StoronaKvadrata / 2;
+                ProbelY = ProbelY + StoronaKvadrata / 2 + StoronaKvadrata;
+            }
+            pictureBox1.Refresh();
+        }
+        void PlaerGreen()
+        {
+            Shirota = pictureBox1.Width;
+            Diametr = Shirota / 14;
+
+            Graphics gr = Graphics.FromImage(pictureBox1.Image);
+
+            ProbelX = Diametr / 2;
+            gr.FillEllipse(new SolidBrush(Color.Green), Shirota / 2 - ProbelX, ProbelX, Diametr, Diametr);
+        }
+        void PlaerRed()
+        {
+            Shirota = pictureBox1.Width;
+            Diametr = Shirota / 14;
+
+            Graphics gr = Graphics.FromImage(pictureBox1.Image);
+
+            ProbelX = Diametr / 2;
+            gr.FillEllipse(new SolidBrush(Color.Red), Shirota / 2 - ProbelX, Shirota - ProbelX - Diametr, Diametr, Diametr);
+        }
+        void StenkaVertik()
+        {
+            Shirota = pictureBox1.Width;
+            StoronaKvadrata = Shirota / 14;
+            ShirotaStenki = Shirota / 14 / 2;
+
+            Graphics gr = Graphics.FromImage(pictureBox1.Image);
+
+            ProbelX = StoronaKvadrata / 2;
+            gr.FillRectangle(Brushes.Gray, StoronaKvadrata + ProbelX, ProbelX, ShirotaStenki, StoronaKvadrata * 2 + ProbelX);
+        }
+
+        void StenkaGorizont()
+        {
+            Shirota = pictureBox1.Width;
+            StoronaKvadrata = Shirota / 14;
+            ShirotaStenki = Shirota / 14 / 2;
+
+            Graphics gr = Graphics.FromImage(pictureBox1.Image);
+
+            ProbelX = StoronaKvadrata / 2;
+            gr.FillRectangle(Brushes.Gray, ProbelX, StoronaKvadrata + ProbelX, StoronaKvadrata * 2 + ProbelX,ShirotaStenki );
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GrafKvadrat();
+            PlaerGreen();
+            PlaerRed();
+            StenkaVertik();
+            StenkaGorizont();
+        }
+    }
+}
